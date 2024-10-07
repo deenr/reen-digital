f6c6429fb661c1820c85ffa0399096e4bc461464
Dean Reymen <deenrayman@outlook.com>
2024-10-07 22:05:34 +0700

    chore: coming soon design

--- src/App.tsx
+++ src/App.tsx
@@ -23,6 +23,7 @@ function App() {
           fill="currentColor"
         />
       </svg>
+      <p className="absolute left-1/2 -translate-x-1/2 bottom-8 font-space-grotesk text-base text-medium text-secondary-900 text-nowrap">© 2024 Reen Digital. All rights reserved.</p>
     </main>
   );
 }
